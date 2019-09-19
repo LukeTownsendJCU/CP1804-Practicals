576a8c0f18f9704c769b1b8b15c4e3ce9a53dcc1
LukeTownsendJCU <luke.townsend@my.jcu.edu.au>
2019-09-19 13:04:00 +1000

    Initial commit

---
+++ prac_06/guitar_test.py
@@ -0,0 +1,15 @@
+"""Client code"""
+from prac_06.guitar import Guitar
+
+
+def main():
+    name = "Gibson L-5 CES"
+    year = 1922
+    cost = 16035.40
+
+    guitar = Guitar(name, year, cost)
+    another = Guitar("Another Guitar", 2012, 66.6)
+    print("{} get_age() - Expected {}. Got {}.".format(guitar.name, 96, guitar.get_age()))
+    print("{} get_age() - Expected {}. Got {}.".format(another.name, 96, another.get_age()))
+    print("{} get_age() - Expected {}. Got {}.".format(guitar.name, True, guitar.is_vintage()))
+    print("{} get_age() - Expected {}. Got {}.".format(another.name, False, another.is_vintage()))
