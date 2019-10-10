4d8e1a24625307c0d7d8f2df699158e151397fb4
LukeTownsendJCU <luke.townsend@my.jcu.edu.au>
2019-10-10 10:01:31 +1000

    Small print statement change

--- prac_08/silver_service_taxi_test.py
+++ prac_08/silver_service_taxi_test.py
@@ -4,7 +4,7 @@ from prac_08.silver_service_taxi import SilverServiceTaxi
 def main():
     silver_taxi = SilverServiceTaxi("Test Taxi", 100, 2)
     silver_taxi.drive(18)
-    print("{}({})".format(silver_taxi, silver_taxi.get_fare()))
+    print("{}. Total fare: ${:.2f}".format(silver_taxi, silver_taxi.get_fare()))
 
 
 main()
