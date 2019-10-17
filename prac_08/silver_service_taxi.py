e42f89abceb143ebf2b906ec843ff71ec9a22a1e
LukeTownsendJCU <luke.townsend@my.jcu.edu.au>
2019-10-17 15:49:49 +1000

    Finished elif and filled in else statement.

--- prac_08/silver_service_taxi.py
+++ prac_08/silver_service_taxi.py
@@ -8,13 +8,13 @@ class SilverServiceTaxi(Taxi):
     flagfall = 4.5
 
     def __init__(self, name, fuel, fanciness):
-        # Does it matter what order these are in.
+        # Does it matter what order these are in?
         super().__init__(name, fuel)
         self.fanciness = fanciness
         self.price_per_km *= fanciness
 
     def __str__(self):
-        return "${} flagfall and taxi fare for {}".format(self.flagfall, super().__str__())
+        return "{} with a flag fall of ${}".format(super().__str__(), self.flagfall)
 
     def get_fare(self):
         # Are we doing the maths in the return because its not a variable we need to keep track of?
--- prac_08/taxi_simulator.py
+++ prac_08/taxi_simulator.py
@@ -11,24 +11,32 @@ from prac_08.silver_service_taxi import SilverServiceTaxi
 
 
 def main():
+    total_fare_cost = 0
     current_taxi = None
     taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2), SilverServiceTaxi("Hummer", 200, 4)]
     menu_choice = input("(C)hoose Taxi, (D)rive, (Q)uit Program").lower()
     while menu_choice != "q":
         if menu_choice == "c":
-            print("Taxi Choices:")
+            print("Available Taxis:")
             taxis_display(taxis)
             taxi_choice = int(input("Choose taxi by its number: "))
             current_taxi = taxis[taxi_choice]
         elif menu_choice == "d":
             current_taxi.start_fare()
-            distance_of_drive = int(input("How far would you like to go?\nPlease enter numerical value:"))
+            distance_of_drive = float(input("How far would you like to go?\nPlease enter numerical value:"))
             current_taxi.drive(distance_of_drive)
-            fare_cost = current_taxi.
+            fare_cost = current_taxi.get_fare()
+            print("Your {} fare will cost you {:.2f}.".format(current_taxi.name, fare_cost))
+            total_fare_cost += fare_cost
 
         else:
             print("Invalid menu option.")
+            print("Current bill = {:.2f}.".format(total_fare_cost))
+            menu_choice = input("(C)hoose Taxi, (D)rive, (Q)uit Program").lower()
 
+    print("Total cost of your trip is {:.2f}.")
+    print("The taxis are now:")
+    taxis_display(taxis)
 
 
 def taxis_display(taxis):
